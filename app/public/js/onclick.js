da3a6a224594e27dc96711ea62bd635ebe4fc182
ClementBallet <clementballet@gmail.com>
2017-04-10 11:50:39 +0200

    maj2 app

--- app/public/js/onclick.js
+++ app/public/js/onclick.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-  $("#button1").click(function(event) {
-    $(this).toggleClass('graphic');
+  $("#graphic-button").click(function() {
+    $( "#graphic" ).replaceWith( "<div class='col-sm-9' id='graphic'></div>" );
+    $( "#graphic-button" ).toggleClass( "active" );
+    $( "#table-button" ).removeClass('active');
+  });
+
+  $("#table-button").click(function() {
+    $( "#graphic" ).replaceWith( "<div class='col-sm-9'>TEST</div>" );
+    $( "#table-button" ).toggleClass( "active" );
+    $( "#graphic-button" ).removeClass('active');
   });
 });
